982461f688fc64d7175217b91149b3153bd8518c
vidurpuri <vidur0610.puri@gmail.com>
2019-02-23 14:52:06 +0530

    End of Section 7

---
+++ DatingApp-SPA/src/app/routes.ts
@@ -0,0 +1,24 @@
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./home/home.component";
+import { MemberListsComponent } from "./member-lists/member-lists.component";
+import { MessagesComponent } from "./messages/messages.component";
+import { ListsComponent } from "./lists/lists.component";
+import { AuthGuard } from "./_guards/auth.guard";
+
+export const appRoutes: Routes = [
+  { path: "", component: HomeComponent },
+  {
+    path: "",
+    runGuardsAndResolvers: "always",
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: "members",
+        component: MemberListsComponent
+      },
+      { path: "messages", component: MessagesComponent },
+      { path: "list", component: ListsComponent }
+    ]
+  },
+  { path: "**", redirectTo: "", pathMatch: "full" }
+];
